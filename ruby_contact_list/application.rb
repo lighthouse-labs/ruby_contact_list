7c27a15d89da438727f1110b97c2a6f7baa1779e
Khurram Virani <kvirani@gmail.com>
2014-01-21 16:54:43 -0800

    Refactorings we did in class together

--- ruby_contact_list/application.rb
+++ ruby_contact_list/application.rb
@@ -1,79 +1,68 @@
 class Application
  
-  def initialize
-    # Start with an empty array of contacts.
-    # TODO: Perhaps stub (seed) some contacts so we don't have to create some every time we restart the app
-    @contacts = []
-  end
+  # def initialize
+  #   # Start with an empty array of contacts.
+  #   # TODO: Perhaps stub (seed) some contacts so we don't have to create some every time we restart the app
+  #   #@contacts = []
+  # end
  
   def run
-    input = ""
-    set_quit = false
+    create_stub_data # temporary
+    # set_quit = false
 
-    until set_quit == true
-      if input == ""
-        input = " "
-        show_main_menu
-        input = gets.chomp
-      end
+    while true
+      show_main_menu
+      input = get_input
 
-      if input == "quit"
-        set_quit = true
-        abort("Have a nice day!")
-      end
+      #binding.pry
+
+      abort("Have a nice day!") if input == "quit"
 
       if input == "new"
-        puts "What is the first name?"
-        input_first_name = gets.chomp
-        puts "What is the last name?"
-        input_last_name = gets.chomp
-        puts "What is the email?"
-        input_email = gets.chomp
-        Contact.new(input_first_name,input_last_name,input_email)
-        input = ""
-      end
-
-      if input == "list"
-        #puts Contact.all
-        #Contact.to_s
-        Contact.instances.each do |thing|
+        new_contact_creation
+      elsif input == "list"
+        Contact.all.each do |thing|
           print thing
         end 
-        input = ""
-      end
-
-      if input == "show"
+      elsif input == "show"
         #puts Contact.all
         #Contact.to_s
         puts "Which id?"
-        id_selected = gets.chomp.to_i
-        print Contact.show_id(id_selected)
+        id_selected = get_input
+.to_i
+        contact = Contact.get(id_selected)
+        puts "Contact details below:"
+        puts contact # automatic conversion to string
         puts "Do you want to edit this contact? y or n?"
-        if gets.chomp == "y"
+        if get_input == "y"
           puts "What do you want to edit? fname, lname, or email?"
-          choice = gets.chomp
+          choice = get_input
+
           case choice
             when "fname"
               puts "New first name?"
-              new_first_name = gets.chomp
-              Contact.change_first_name(id_selected,new_first_name)
+              new_first_name = get_input
+
+              contact.first_name  = new_first_name
             when "lname"
               puts "New last name?"
-              new_last_name = gets.chomp
-              Contact.change_last_name(id_selected,new_last_name)
+              new_last_name = get_input
+
+              contact.last_name  = new_last_name
             when "email"
               puts "New email?"
-              new_email = gets.chomp
-              Contact.change_email(id_selected,new_email)
+              new_email = get_input
+
+              contact.email = new_email
           end
         end
-        input = ""
       end
-
     end
 
   end
-  
+
+  private 
+
   # Prints the main menu only
   def show_main_menu
     puts "Welcome to the app. What's next?"
@@ -82,6 +71,36 @@ class Application
     puts " show :id - Display contact details"
     puts " quit     - Quit the application"
     print "> "
+  end
+
+  def get_contact_information
+    puts "What is the first name?"
+    first_name = get_input
+
+    puts "What is the last name?"
+    last_name = get_input
+
+    puts "What is the email?"
+    email = get_input
+
+    [first_name, last_name, email]
+  end
+
+  def new_contact_creation
+    first_name, last_name, email = get_contact_information
+    Contact.create(first_name, last_name, email)
+  end
+
+  def create_stub_data
+    Contact.create("Tom", "Sav", "tomsav2010@gmail.com")
+    Contact.create("Lauren","Hearnden","Lauren@gmail.com")
+    Contact.create("Moo","Cow","Moo@Cow.com")
+    Contact.create("Woof","Dog","Woof@Dog.com")
+    Contact.create("Meow","Cat","Meow@Cat.com")
+  end
+
+  def get_input
+    gets.chomp.strip
   end
  
 end
--- ruby_contact_list/contact.rb
+++ ruby_contact_list/contact.rb
@@ -3,77 +3,53 @@ class Contact
   attr_accessor :last_name
   attr_accessor :email
 
-  @@count = 0
   @@instances = []
-
-  def self.doSomething
-  	
-  end
-
-  def check_duplicate(email)
-    @@instances.each do |contact|
-      if (email == contact.email)
-        return false
-      end
-    end  
-
-    true
-  end
 
   def initialize(first_name, last_name, email)
     # TODO: assign local variables to instance variables
-    if ( check_duplicate(email) ) 
-	    @first_name = first_name
-	    @last_name = last_name
-	    @email = email
-	    @id = @@count
-	    @@count += 1
-	    @@instances << self
-		end	
+    @first_name = first_name
+    @last_name = last_name
+    @email = email
+    @id = Contact.count
   end
 
-
-
-  def to_s
-    puts "id: #{@id} #{@first_name} #{@last_name} (#{@email})\n"
+  # Contact.having_email?(email)
+  # class method
+  def self.email_taken?(email)
+    @@instances.detect { |contact| contact.email == email }
   end
 
-  def self.show_id(id)
-  	if id < @@count
-
-  		puts instances[id]
-  	end
+  # Contact.create
+  def self.create(first, last, email)
+    unless email_taken?(email)
+      contact = new(first, last, email)
+      @@instances << contact
+    end
   end
 
-  def self.change_first_name(id,first_name)
-  	instances[id].first_name = first_name
-  end
-  
-  def self.change_last_name(id,last_name)
-  	instances[id].last_name = last_name
-  end
-
-  def self.change_email(id,email)
-  	instances[id].email = email
+  def self.get(id)
+  	@@instances[id.to_i]
   end
 
   def self.all
-  	@@instances.inspect
+  	@@instances
   end
 
   def self.count
-  	@@count
+  	@@instances.size
   end
 
   def self.instances
   	@@instances
   end
+
+  def to_s
+    "id: #{@id} #{@first_name} #{@last_name} (#{@email})\n"
+  end
+
+  private
+
+  # private methods here
   
 end
-
-c0 = Contact.new("Tom", "Sav", "tomsav2010@gmail.com")
-c1 = Contact.new("Lauren","Hearnden","Lauren@gmail.com")
-c2 = Contact.new("Moo","Cow","Moo@Cow.com")
-c3 = Contact.new("Woof","Dog","Woof@Dog.com")
-c4 = Contact.new("Meow","Cat","Meow@Cat.com")
 
